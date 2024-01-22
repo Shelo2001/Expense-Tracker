d7d6402ccc56a7bca64ec7c3525785403ca83299
shelo2001 <sheliaakaki47@gmail.com>
2024-01-22 18:07:07 +0400

    added delete category functionality

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -24,23 +24,6 @@ namespace ExpenseTracker.Controllers
             return View(await _context.Categories.ToListAsync());
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
 
         // GET: Category/AddOrEdit
         public IActionResult AddOrEdit(int id=0)
@@ -75,27 +58,6 @@ namespace ExpenseTracker.Controllers
             return View(category);
         }
 
-
-
-
-
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
 
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
